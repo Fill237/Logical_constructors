d409257833334319fa8cb6122db8737242dd6777
Fill237 <andreeva.mariya94@bk.ru>
2022-02-09 12:08:57 +0300

    task_4

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner src = new Scanner(System.in);
+        int x = src.nextInt();
+        int y = src.nextInt();
+        int z = src.nextInt();
+        double summ = (x + y + z)/3;
+        System.out.println("Среднее арифмитическое чисел равно: " + summ);
+        double summ1 = Math.floor(summ/2);                                  // округляем в меньшую сторону
+        int avr = (int) summ1;                                              // double to int
+        if (avr > 3){
+            System.out.println("Программа выполнена корректно");
+        } else
+            System.out.println("Попробуйте ввести другие значения");
+    }
+}
